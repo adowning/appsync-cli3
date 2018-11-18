ad89b88cfb2bbaebac3d004b6525d8d39528836f
adowning <ashdowning@gmail.com>
2018-11-18 14:20:43 -0600

    Scripted auto-commit on change (2018-11-18 14:20:43) by gitwatch.sh

--- src/vuex/modules/users/store.js
+++ src/vuex/modules/users/store.js
@@ -1,34 +1,37 @@
-import * as types from "./mutation-types";
+import * as types from './mutation-types';
 // import kuzzle from "../../services/kuzzle";
-import { kuzzleWrapper } from "../../../services/kuzzleWrapper";
+import {
+  performSearchUsers,
+  performSearchTimesheets
+} from '../../../services/kuzzleWrapper';
 
 const state = {
   users: [],
   timeSheets: []
-};
+}
 
 const actions = {
   async [types.UPDATE_USERLIST]({ commit, dispatch }) {
-    var data = await kuzzleWrapper.performSearchUsers(null, null, {}, 50, []);
-    commit(types.USERLIST, data);
+    var users = await performSearchUsers(null, null, {}, 50, [])
+    commit(types.USERLIST, users)
   },
-  async [types.UPDATE_TIMESHEETS]({ commit, dispatch }, data) {
-    var data = await kuzzleWrapper.performSearchTimesheets;
-    commit(types.TIMESHEETS, data);
+  async [types.UPDATE_TIMESHEETS]({ commit, dispatch }) {
+    var timeSheets = await performSearchTimesheets({}, 50)
+    commit(types.TIMESHEETS, timeSheets)
   }
-};
+}
 
 const mutations = {
   [types.USERLIST](state, data) {
-    state.users = data;
+    state.users = data
   },
   async [types.TIMESHEETS](state, data) {
-    state.timeSheets = data;
+    state.timeSheets = data
   }
-};
+}
 
 export default {
   state,
   mutations,
   actions
-};
+}
