606670ac5e9b419056b158eefcc8f5d83b59b9a4
adowning <ashdowning@gmail.com>
2018-11-19 10:55:18 -0600

    Scripted auto-commit on change (2018-11-19 10:55:18) by gitwatch.sh

---
+++ src/components/Crew/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="Crew">crew</div>
+</template>
+
+<script>
+/* This Component is a template. */
+
+export default {
+  name: 'Crew',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {},
+  watch: {}
+}
+</script>
+
+<style scoped lang="scss">
+.Component {
+}
+</style>
