fe2f2c588d3f6f3d3a43329ac557ddc49ce0325e
adowning <ashdowning@gmail.com>
2018-11-19 10:10:32 -0600

    Scripted auto-commit on change (2018-11-19 10:10:32) by gitwatch.sh

--- src/components/Profile/address.vue
+++ src/components/Profile/address.vue
@@ -3,94 +3,91 @@
     <v-card-text class="pl-4 pr-4">
       <div class="tool a-0 ma-0">
         <div class="caption mb-1">
-          <v-icon
-            small
-            class="mr-1">location_on</v-icon>
+          <v-icon small class="mr-1">location_on</v-icon>
           {{ caption }}
         </div>
-        <v-spacer/>
+        <v-spacer />
         <v-btn
           icon
           flat
           small
           class="pa-0 ma-0 topright"
-          @click="showEditView = !showEditView">
-          <v-icon
-            v-if="addressUp !== ''"
-            small
-            color="indigo lighten-1">edit</v-icon>
-          <v-icon
-            v-else
-            small
-            color="indigo lighten-1">mdi-plus-circle-outline</v-icon>
+          @click="showEditView = !showEditView;"
+        >
+          <v-icon v-if="addressUp !== ''" small color="indigo lighten-1"
+            >edit</v-icon
+          >
+          <v-icon v-else small color="indigo lighten-1"
+            >mdi-plus-circle-outline</v-icon
+          >
         </v-btn>
       </div>
-      <div
-        v-if="addressUp === ''"
-        class="body-2">...</div>
-      <div
-        v-else
-        class="body-2">
+      <div v-if="addressUp === ''" class="body-2">...</div>
+      <div v-else class="body-2">
         <div v-if="addressUpdate.line !== ''">{{ addressUpdate.line }}</div>
-        <div v-if="addressUpdate.city + addressUpdate.state + addressUpdate.zipcode + addressUpdate.country !== ''">
-          {{ addressUpdate.city + ' ' + addressUpdate.state + ' ' + addressUpdate.zipcode + ' ' + addressUpdate.country }}
+        <div
+          v-if="
+            addressUpdate.city +
+              addressUpdate.state +
+              addressUpdate.zipcode +
+              addressUpdate.country !==
+              ''
+          "
+        >
+          {{
+            addressUpdate.city +
+              " " +
+              addressUpdate.state +
+              " " +
+              addressUpdate.zipcode +
+              " " +
+              addressUpdate.country
+          }}
         </div>
       </div>
     </v-card-text>
-    <div
-      v-if="showEditView"
-      class="pt-2 pl-2 pr-2 pb-2 indigo lighten-5">
+    <div v-if="showEditView" class="pt-2 pl-2 pr-2 pb-2 indigo lighten-5">
       <v-card-text class="indigo lighten-5">
-        <v-text-field
-          v-model="addressUpdate.line"
-          label="Address Line"/>
-        <v-layout
-          row
-          wrap>
+        <v-text-field v-model="addressUpdate.line" label="Address Line" />
+        <v-layout row wrap>
           <v-flex>
-            <v-text-field
-              v-model="addressUpdate.city"
-              label="Town/City"/>
+            <v-text-field v-model="addressUpdate.city" label="Town/City" />
           </v-flex>
           <v-flex>
             <v-text-field
               v-model="addressUpdate.state"
-              label="Province/State"/>
+              label="Province/State"
+            />
           </v-flex>
         </v-layout>
-        <v-layout
-          row
-          wrap>
+        <v-layout row wrap>
           <v-flex>
-            <v-text-field
-              v-model="addressUpdate.zipcode"
-              label="Zip Code"/>
+            <v-text-field v-model="addressUpdate.zipcode" label="Zip Code" />
           </v-flex>
-          <v-flex >
+          <v-flex>
             <v-select
               :items="countries"
               v-model="addressUpdate.country"
               label="Country"
-              autocomplete/>
+              autocomplete
+            />
           </v-flex>
         </v-layout>
       </v-card-text>
       <v-card-actions>
-        <v-btn
-          :disabled="!enableSave"
-          class="ml-4"
-          small
-          @click="cancelEdit()">CANCEL</v-btn>
-        <v-spacer/>
-        <v-btn
-          small
-          @click="closeEdit()" >CLOSE</v-btn>
+        <v-btn :disabled="!enableSave" class="ml-4" small @click="cancelEdit();"
+          >CANCEL</v-btn
+        >
+        <v-spacer />
+        <v-btn small @click="closeEdit();">CLOSE</v-btn>
         <v-btn
           :disabled="!enableSave"
           class="mr-4"
           small
           color="success"
-          @click="updateAttribute()">SAVE</v-btn>
+          @click="updateAttribute();"
+          >SAVE</v-btn
+        >
       </v-card-actions>
     </div>
   </div>
@@ -109,16 +106,16 @@ export default {
       menu: false,
       modal: false,
       addressUpdate: {
-        line: '',
-        city: '',
-        state: '',
-        zipcode: '',
-        country: ''
+        line: "",
+        city: "",
+        state: "",
+        zipcode: "",
+        country: ""
       },
       showEditView: false,
       enableSave: false,
       countries: countries.getNames()
-    }
+    };
   },
   computed: {
     addressUp: function() {
@@ -128,10 +125,16 @@ export default {
         this.addressUpdate.state +
         this.addressUpdate.zipcode +
         this.addressUpdate.country
-      )
+      );
     },
     addressProp: function() {
-      return this.address.line + this.address.city + this.address.state + this.address.zipcode + this.address.country
+      return (
+        this.address.line +
+        this.address.city +
+        this.address.state +
+        this.address.zipcode +
+        this.address.country
+      );
     }
   },
   watch: {
@@ -143,29 +146,29 @@ export default {
         this.addressUpdate.zipcode !== this.address.zipcode ||
         this.addressUpdate.country !== this.address.country
       ) {
-        this.enableSave = true
+        this.enableSave = true;
       } else {
-        this.enableSave = false
+        this.enableSave = false;
       }
     },
     addressProp: function() {
-      this.addressUpdate = JSON.parse(JSON.stringify(this.address))
+      this.addressUpdate = JSON.parse(JSON.stringify(this.address));
     }
   },
   methods: {
     cancelEdit: function() {
-      this.addressUpdate = JSON.parse(JSON.stringify(this.address))
+      this.addressUpdate = JSON.parse(JSON.stringify(this.address));
     },
     closeEdit: function() {
-      this.cancelEdit()
-      this.showEditView = false
+      this.cancelEdit();
+      this.showEditView = false;
     },
     updateAttribute: function() {
-      this.$emit('updateAddress', this.addressUpdate)
-      this.enableSave = false
+      this.$emit("updateAddress", this.addressUpdate);
+      this.enableSave = false;
     }
   }
-}
+};
 </script>
 <style scoped>
 .tool {
