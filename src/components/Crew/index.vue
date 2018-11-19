9ab14cacbae06b125de7a0a575079e8ffa772b81
adowning <ashdowning@gmail.com>
2018-11-19 17:43:02 -0600

    Scripted auto-commit on change (2018-11-19 17:43:02) by gitwatch.sh

--- src/components/Crew/index.vue
+++ src/components/Crew/index.vue
@@ -6,39 +6,39 @@
 
 <script>
 /* This Component is a template. */
-import kuzzle from '../../services/kuzzle';
+import kuzzle from "../../services/kuzzle";
 
 export default {
-  name: "Crew",
+  name: 'Crew',
   components: {},
   props: {},
   data() {
-    return {};
+    return {}
   },
   computed: {},
   methods: {
     async test() {
       const query = {
-        controller: "kuzzle-core-plugin-boilerplate/myNewController",
-        action: "getDocument",
-        documentId: "2342344",
-        indexName: "playground",
-        collectionName: "test"
-      }
+        controller: 'kuzzle-core-plugin-boilerplate/myNewController',
+        action: 'myNewAction'
+        // documentId: "2342344",
+        // indexName: "playground",
+        // collectionName: "test"
+      };
 
       try {
-        await kuzzle.connect()
-        const response = await kuzzle.query(query, {})
-        console.log(response._result)
+        await kuzzle.connect();
+        const response = await kuzzle.query(query, {});
+        console.log(response._result);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     }
   },
   created() {},
   mounted() {},
   watch: {}
-};
+}
 </script>
 
 <style scoped lang="scss">
--- src/services/kuzzle.js
+++ src/services/kuzzle.js
@@ -1,23 +1,23 @@
-const { Kuzzle } = require('kuzzle-sdk');
+// const { Kuzzle } = require('kuzzle-sdk');
 
-const kuzzle = new Kuzzle('websocket', { host: 'localhost', port: 7512 });
+// const kuzzle = new Kuzzle('websocket', { host: 'localhost', port: 7512 });
 
-kuzzle
-  .connect()
-  .then(() => {
-    // You are now connected to your Kuzzle instance.
-    return kuzzle.server.now();
-  })
-  .then(serverTime => console.log(serverTime))
-  .catch(error => console.error(error));
+// kuzzle
+//   .connect()
+//   .then(() => {
+//     // You are now connected to your Kuzzle instance.
+//     return kuzzle.server.now();
+//   })
+//   .then(serverTime => console.log(serverTime))
+//   .catch(error => console.error(error));
 
-// import Kuzzle from 'kuzzle-sdk/dist/kuzzle'
-// import Promise from 'bluebird';
-// Kuzzle.prototype.bluebird = Promise
+import Kuzzle from "kuzzle-sdk/dist/kuzzle";
+import Promise from "bluebird";
+Kuzzle.prototype.bluebird = Promise;
 
-// let kuzzle = new Kuzzle("localhost", {
-//   connect: "manual"
-// });
+let kuzzle = new Kuzzle('localhost', {
+  connect: 'manual'
+})
 
-window.kuzzle = kuzzle;
-export default kuzzle;
+window.kuzzle = kuzzle
+export default kuzzle
