a7fe76a15bebe5018261550b2b5cc317e684c9fb
adowning <ashdowning@gmail.com>
2018-11-19 17:32:11 -0600

    Scripted auto-commit on change (2018-11-19 17:32:11) by gitwatch.sh

--- src/services/kuzzle.js
+++ src/services/kuzzle.js
@@ -1,11 +1,14 @@
 // import Kuzzle from 'kuzzle-sdk/dist/kuzzle'
-import Kuzzle from 'kuzzle-sdk/src/Kuzzle';
+import Kuzzle from 'kuzzle-sdk';
 import Promise from 'bluebird';
 Kuzzle.prototype.bluebird = Promise
 
-let kuzzle = new Kuzzle('localhost', {
-  connect: 'manual'
-})
+// let kuzzle = new Kuzzle("localhost", {
+//   connect: "manual"
+// });
 
-window.kuzzle = kuzzle
-export default kuzzle
+// window.kuzzle = kuzzle;
+// export default kuzzle;
+
+window.kuzzle = Kuzzle
+export default Kuzzle
