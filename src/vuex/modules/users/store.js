dd5b94eb0526e3ea09daab9eb76a4ba6a18869aa
adowning <ashdowning@gmail.com>
2018-11-20 14:48:50 -0600

    Scripted auto-commit on change (2018-11-20 14:48:50) by gitwatch.sh

--- src/vuex/modules/users/store.js
+++ src/vuex/modules/users/store.js
@@ -1,32 +1,32 @@
-import * as types from "./mutation-types";
+import * as types from './mutation-types';
 // import kuzzle from "../../services/kuzzle";
 import {
   performSearchUsers,
   performSearchDocuments,
   clockInUser
-} from "../../../services/kuzzleWrapper";
+} from '../../../services/kuzzleWrapper';
 
 const state = {
   users: [],
   timeSheets: [],
   deviceUserlist: []
-};
+}
 
 const actions = {
   async [types.UPDATE_USERLIST]({ commit, dispatch }) {
-    var users = await performSearchUsers(null, null, {}, 50, {});
-    commit(types.USERLIST, users);
+    var users = await performSearchUsers(null, null, {}, 50, {})
+    commit(types.USERLIST, users)
   },
   async [types.DEVICE_USERLIST]({ commit, dispatch }) {
     var list = await performSearchDocuments(
-      "timeSheets",
-      "playground",
+      'timeSheets',
+      'playground',
       // [{
       {
         query: {
           bool: {
             must: [
-              { match: { out_timestamp: '0' } }
+              { match: { out_timestamp: "0" } }
               // { match: { "employee.id": '1444044' } }
             ]
           }
@@ -36,14 +36,14 @@ const actions = {
       // ],
       50,
       []
-    );
-    commit(types.DEVICE_USERLIST, list);
+    )
+    commit(types.DEVICE_USERLIST, list)
   },
   async [types.DO_CLOCKIN]({ commit, dispatch }, id) {
     // var status = await clockInUser(id);
-    var status = await clockInUser(id);
-    console.log(status)
-    return status;
+    var status = await clockInUser(id)
+    console.log(status);
+    return status
     // var timeSheets = await performSearchDocuments(
     //   'timeSheets',
     //   'playground',
@@ -55,30 +55,30 @@ const actions = {
   },
   async [types.UPDATE_TIMESHEETS]({ commit, dispatch }) {
     var timeSheets = await performSearchDocuments(
-      "timeSheets",
-      "playground",
+      'timeSheets',
+      'playground',
       {},
       50,
       {}
-    );
-    commit(types.TIMESHEETS, timeSheets);
+    )
+    commit(types.TIMESHEETS, timeSheets)
   }
-};
+}
 
 const mutations = {
   [types.USERLIST](state, data) {
-    state.users = data;
+    state.users = data
   },
   [types.DEVICE_USERLIST](state, data) {
-    state.deviceUserlist = data;
+    state.deviceUserlist = data
   },
   async [types.TIMESHEETS](state, data) {
-    state.timeSheets = data;
+    state.timeSheets = data
   }
-};
+}
 
 export default {
   state,
   mutations,
   actions
-};
+}
