218384aa6ff0c430d06c5a543104b719aae82f06
adowning <ashdowning@gmail.com>
2018-11-20 13:31:41 -0600

    Scripted auto-commit on change (2018-11-20 13:31:41) by gitwatch.sh

--- src/components/Crew/index.vue
+++ src/components/Crew/index.vue
@@ -12,47 +12,31 @@
           <v-list two-line>
             <template v-for="(item, index) in users">
               <v-list-tile :key="item.id" avatar ripple @click="toggle(index);">
-                <!--
-                  <v-list-tile-avatar>
-                    <img :src="item.id" />
-                  </v-list-tile-avatar>
-                  <v-list-tile-content>
-                    <v-list-tile-title>{{ item.id }}</v-list-tile-title>
-                    <v-list-tile-title>{{ item.id }}</v-list-tile-title>
-                    <v-list-tile-sub-title class="text--primary">{{
-                      item.id
-                    }}</v-list-tile-sub-title>
-                    <v-list-tile-sub-title>{{ item.id }}</v-list-tile-sub-title>
-                  </v-list-tile-content>
+                <v-list-tile-avatar>
+                  <img :src="item.id" />
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ item.id }}</v-list-tile-title>
+                  <v-list-tile-title>{{ item.id }}</v-list-tile-title>
+                  <v-list-tile-sub-title class="text--primary">{{
+                    item.id
+                  }}</v-list-tile-sub-title>
+                  <v-list-tile-sub-title>{{ item.id }}</v-list-tile-sub-title>
+                </v-list-tile-content>
 
-                  <v-list-tile-action>
-                    <v-list-tile-action-text>{{
-                      item.id
-                    }}</v-list-tile-action-text>
-                    <v-icon
-                      v-if="selected.indexOf(index) < 0"
-                      color="grey lighten-1"
-                    >
-                      star_border
-                    </v-icon>
+                <v-list-tile-action>
+                  <v-list-tile-action-text>{{
+                    item.id
+                  }}</v-list-tile-action-text>
+                  <v-icon
+                    v-if="selected.indexOf(index) < 0"
+                    color="grey lighten-1"
+                  >
+                    fa-star_border
+                  </v-icon>
 
-                    <v-icon v-else color="yellow darken-2"> star </v-icon>
-                  </v-list-tile-action>
-                  <v-list-tile-action>
-                    <v-list-tile-action-text>{{
-                      item.id
-                    }}</v-list-tile-action-text>
-                        <v-icon
-                          v-if="selected.indexOf(index) < 0"
-                          color="grey lighten-1"
-                        >
-                          star_border
-                        </v-icon>
-
-                      <v-icon v-else color="yellow darken-2"> star </v-icon>
-
-                  </v-list-tile-action>
-                -->
+                  <v-icon v-else color="yellow darken-2"> fa-star </v-icon>
+                </v-list-tile-action>
               </v-list-tile>
 
               <v-divider
@@ -88,7 +72,9 @@ export default {
   components: {},
   props: {},
   data() {
-    return {};
+    return {
+      selected: [0]
+    };
   },
   computed: {
     ...mapState({
@@ -97,6 +83,15 @@ export default {
     })
   },
   methods: {
+    toggle(index) {
+      const i = this.selected.indexOf(index);
+
+      if (i > -1) {
+        this.selected.splice(i, 1);
+      } else {
+        this.selected.push(index);
+      }
+    },
     async test4() {
       this.$store
         .dispatch(DO_CLOCKIN, '1444044')
