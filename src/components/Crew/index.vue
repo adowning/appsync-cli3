ee37631163618685573dba598b14cb0c929da916
adowning <ashdowning@gmail.com>
2018-11-20 13:12:15 -0600

    Scripted auto-commit on change (2018-11-20 13:12:15) by gitwatch.sh

--- src/components/Crew/index.vue
+++ src/components/Crew/index.vue
@@ -1,5 +1,74 @@
 <template>
   <div class="Crew">
+    <v-layout row>
+      <v-flex xs12>
+        <v-card>
+          <v-toolbar color="green" dark>
+            <!-- <v-toolbar-side-icon></v-toolbar-side-icon> -->
+            <v-toolbar-title>Crew list</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon> <v-icon>menu</v-icon> </v-btn>
+          </v-toolbar>
+
+          <v-list two-line>
+            <template v-for="(item, index) in items">
+              <v-list-tile
+                :key="item.title"
+                avatar
+                ripple
+                @click="toggle(index);"
+              >
+                <v-list-tile-avatar>
+                  <img :src="item.avatar" />
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                  <v-list-tile-sub-title class="text--primary">{{
+                    item.headline
+                  }}</v-list-tile-sub-title>
+                  <v-list-tile-sub-title>{{
+                    item.subtitle
+                  }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+
+                <v-list-tile-action>
+                  <v-list-tile-action-text>{{
+                    item.action
+                  }}</v-list-tile-action-text>
+                  <v-icon
+                    v-if="selected.indexOf(index) < 0"
+                    color="grey lighten-1"
+                  >
+                    star_border
+                  </v-icon>
+
+                  <v-icon v-else color="yellow darken-2"> star </v-icon>
+                </v-list-tile-action>
+                <v-list-tile-action>
+                  <v-list-tile-action-text>{{
+                    item.action
+                  }}</v-list-tile-action-text>
+                  <v-icon
+                    v-if="selected.indexOf(index) < 0"
+                    color="grey lighten-1"
+                  >
+                    star_border
+                  </v-icon>
+
+                  <v-icon v-else color="yellow darken-2"> star </v-icon>
+                </v-list-tile-action>
+              </v-list-tile>
+
+              <v-divider
+                v-if="index + 1 < items.length"
+                :key="index"
+              ></v-divider>
+            </template>
+          </v-list>
+        </v-card>
+      </v-flex>
+    </v-layout>
     <v-btn block outline warning @click="test();">clock in</v-btn>
     <v-btn block outline warning @click="test2();">clock out</v-btn>
     <v-btn block outline warning @click="test3();"> test</v-btn>
@@ -9,82 +78,83 @@
 
 <script>
 /* This Component is a template. */
-import kuzzle from '../../services/kuzzle';
-import { DO_CLOCKIN } from '../../vuex/modules/users/mutation-types';
+import kuzzle from "../../services/kuzzle";
+import { DO_CLOCKIN } from "../../vuex/modules/users/mutation-types";
 import {
   UPDATE_USERLIST,
   UPDATE_TIMESHEETS
-} from '../../vuex/modules/users/mutation-types';
+} from "../../vuex/modules/users/mutation-types";
 
 export default {
-  name: "Crew",
+  name: 'Crew',
   components: {},
   props: {},
   data() {
-    return {};
+    return {}
   },
   computed: {},
   methods: {
     async test4() {
       this.$store
-        .dispatch(DO_CLOCKIN, '1444044')
+        .dispatch(DO_CLOCKIN, "1444044")
         .then(() => {
-          this.onLogin();
+          this.onLogin()
         })
         .catch(err => {
-          this.error = err.message;
-        });
+          this.error = err.message
+        })
     },
     async test() {
       const args = {
-          controller: "kuzzle-core-plugin-boilerplate/clockPunch",
-          action: "clockIn"
+          controller: 'kuzzle-core-plugin-boilerplate/clockPunch',
+          action: 'clockIn'
         },
         query = {
-          empId: "1444044"
-        }
+          empId: '1444044'
+        };
       kuzzle.query(args, query, function(err, res) {
-        console.log(err)
-        console.log(res.result)
-      })
+        console.log(err);
+        console.log(res.result);
+      });
     },
     async test2() {
       const args = {
-          controller: "kuzzle-core-plugin-boilerplate/clockPunch",
-          action: "clockOut"
+          controller: 'kuzzle-core-plugin-boilerplate/clockPunch',
+          action: 'clockOut'
         },
         query = {
-          empId: "1444044"
-        }
+          empId: '1444044'
+        };
       kuzzle.query(args, query, function(err, res) {
-        console.log(err)
-        console.log(res)
-      })
+        console.log(err);
+        console.log(res);
+      });
     },
     async test3() {
       const args = {
-          controller: "kuzzle-core-plugin-boilerplate/clockPunch",
-          action: "test"
+          controller: 'kuzzle-core-plugin-boilerplate/clockPunch',
+          action: 'test'
         },
         query = {
-          empId: "1444044"
-        }
+          empId: '1444044'
+        };
       kuzzle.query(args, query, function(err, res) {
-        console.log(err)
-        console.log(res.result)
-      })
+        console.log(err);
+        console.log(res.result);
+      });
     }
   },
   created() {},
   mounted() {
-    this.$store.dispatch(UPDATE_USERLIST, {});
-    this.$store.dispatch(UPDATE_TIMESHEETS, {})
+    this.$store.dispatch(UPDATE_USERLIST, {})
+    this.$store.dispatch(UPDATE_TIMESHEETS, {});
   },
   watch: {}
-};
+}
 </script>
 
-<style scoped lang="scss">
-.Crew {
+<style scoped>
+.v-list--two-line .v-list__tile {
+  height: 122px;
 }
 </style>
