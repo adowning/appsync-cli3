b0a0057680bcf5a94c5ea0db19382ad1fe203e4b
adowning <ashdowning@gmail.com>
2018-11-21 11:13:42 -0600

    Scripted auto-commit on change (2018-11-21 11:13:42) by gitwatch.sh

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,16 @@
 // const path = require('path');
 
 module.exports = {
-  baseUrl: "/",
-  outputDir: "server/www/dist",
+  baseUrl: '/',
+  outputDir: 'server/www/dist',
   lintOnSave: true,
   runtimeCompiler: false,
   configureWebpack: {
-    devtool: "source-map"
+    devtool: 'source-map'
+  },
+  devServer: {
+    host: "0.0.0.0",
+    hot: true,
+    disableHostCheck: true
   }
-};
+}
