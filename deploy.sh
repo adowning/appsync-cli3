1f3ed99fc006d5db6ea8fb5e5b2df300fa785b6b
adowning <ashdowning@gmail.com>
2018-11-27 10:59:20 -0600

    Scripted auto-commit on change (2018-11-27 10:59:20) by gitwatch.sh

--- deploy.sh
+++ deploy.sh
@@ -11,18 +11,19 @@ npm run build
 
 
 # navigate into the build output directory
-cd dist
+# cd dist
 
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-
-git init
+DATE=`date`
+# git init
 git add -A
-git commit -m 'deploy'
+git commit -m $DATE
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:adowning/adowning.github.io.git master
+# git push -f git@github.com:adowning/adowning.github.io.git master
+git subtree push --prefix dist origin gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 #git push -f git@github.com:adowning/appsync-cli3.git master:gh-pages
