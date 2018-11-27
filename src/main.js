35c971ecd78fc7b88aa9da5f8be58e386489058d
adowning <ashdowning@gmail.com>
2018-11-27 05:21:16 -0600

    Scripted auto-commit on change (2018-11-27 05:21:16) by gitwatch.sh

--- src/main.js
+++ src/main.js
@@ -1,42 +1,42 @@
-import Vue from 'vue';
-import './plugins/vuetify';
-import VueRouter from 'vue-router';
-import App from './App';
-import store from './vuex/store';
-import { initStoreWithKuzzle } from './services/kuzzleWrapper';
-import * as types from './vuex/modules/common/kuzzle/mutation-types';
-import 'roboto-fontface/css/roboto/roboto-fontface.css';
-import '@fortawesome/fontawesome-free/css/all.css';
+import Vue from 'vue'
+import './plugins/vuetify'
+import VueRouter from 'vue-router'
+import App from './App'
+import store from './vuex/store'
+import { initStoreWithKuzzle } from './services/kuzzleWrapper'
+import * as types from './vuex/modules/common/kuzzle/mutation-types'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 if (process.env.NODE_ENV == 'development') {
-  Vue.prototype.inDevelopment = true
+    Vue.prototype.inDevelopment = true
 }
 initStoreWithKuzzle(store)
-
+console.log(process.env)
 Vue.use(VueRouter)
 console.log(navigator.userAgent)
 
 store.dispatch(types.LOAD_ENVIRONMENTS)
 store
-  .dispatch(types.SWITCH_LAST_ENVIRONMENT)
-  .then(() => {
-    let router = require('./services/router').default
+    .dispatch(types.SWITCH_LAST_ENVIRONMENT)
+    .then(() => {
+        let router = require('./services/router').default
 
-    /* eslint-disable no-new */
-    new Vue({
-      el: '#app',
-      router,
-      store,
-      render: h => h(App)
+        /* eslint-disable no-new */
+        new Vue({
+            el: '#app',
+            router,
+            store,
+            render: h => h(App),
+        })
     })
-  })
-  .catch(() => {
-    let router = require('./services/router').default
+    .catch(() => {
+        let router = require('./services/router').default
 
-    /* eslint-disable no-new */
-    new Vue({
-      el: '#app',
-      router,
-      store,
-      render: h => h(App)
+        /* eslint-disable no-new */
+        new Vue({
+            el: '#app',
+            router,
+            store,
+            render: h => h(App),
+        })
     })
-  });
