5bd2615dd27763338119106b2d6ab40f0711c5d3
adowning <ashdowning@gmail.com>
2018-11-27 10:29:47 -0600

    Scripted auto-commit on change (2018-11-27 10:29:47) by gitwatch.sh

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,11 @@ set -e
 
 # build
 npm run build
+echo %s > force.txt
 
 # navigate into the build output directory
 cd dist
+
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -21,5 +23,5 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:adowning/appsync-cli3.git master:gh-pages
-
+rm force.txt
 cd -
